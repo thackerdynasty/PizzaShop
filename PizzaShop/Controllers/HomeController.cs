973d54bfb43a4d4a20d1461a95b8d0aab6b34681
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2024-02-21 17:03:56 -0600

    2

--- PizzaShop/Controllers/HomeController.cs
+++ PizzaShop/Controllers/HomeController.cs
@@ -6,26 +6,30 @@ namespace PizzaShop.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+	private readonly ILogger<HomeController> _logger;
+	AppDBContext context;
+	
+	public HomeController(AppDBContext context, ILogger<HomeController> logger)
+	{
+		_logger = logger;
+		this.context = context;
+	}
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+	public IActionResult Index()
+	{
+		ViewData["hasOrder"] = OrderController.checkForOrder(context);
+		return View();
+	}
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+	public IActionResult Privacy()
+	{
+		ViewData["hasOrder"] = OrderController.checkForOrder(context);
+		return View();
+	}
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
--- PizzaShop/Program.cs
+++ PizzaShop/Program.cs
@@ -1,16 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaShop.Models;
+using Microsoft.EntityFrameworkCore.Sqlite;
+var serviceProvider = new ServiceCollection().AddDbContext<AppDBContext>(options => options.UseSqlite("Data Source=PizzaShop.db")).BuildServiceProvider();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlite("Data Source=PizzaShop.db"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -21,7 +27,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
